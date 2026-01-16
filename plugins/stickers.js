82f9ccf03277738b6402f068062163f2f4695526
Ostyado <clamourostyado@gmail.com>
2026-01-16 19:10:10 +0300

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -1,38 +1,50 @@
 require("../settings");
-let handler = async (m,{res,client, text, prefix,pushname, reaction}) => {
-const quoted = m.quoted || m.msg?.contextInfo?.quotedMessage;
-const mime =
-        quoted.mimetype ||
-        quoted.msg?.mimetype ||
-        '';
-                const swn = text || `${pushname}`
-                const pcknm = swn.split("|")[0]
-                const atnm = swn.split("|")[1]
-                if (m.quoted.isAnimated === true) {
-                   let media1 = client.downloadAndSaveMediaMessage(quoted, "gifee")
-                    client.sendMessage(m.chat, { 
-                        sticker: media1 }, m, {
-                        packname: pcknm,
-                        author: atnm
-                    })
-                } else if (/image/.test(mime)) {
-                    let media = await quoted.download()
-                    let encmedia = await client.sendImageAsSticker(m.chat, media, m, {
-                        packname: pcknm,
-                        author: atnm
-                    })
-                    } else if (/video/.test(mime)) {
-                        if ((quoted.msg || quoted).seconds > 10) return res('\n𝙼𝚊𝚡 𝟷𝟶 𝚜𝚎𝚌𝚜\n')
-                        let media = await quoted.download()
-                        let encmedia = await client.sendVideoAsSticker(m.chat, media, m, {
-                            packname: pcknm, 
-                            author: atnm
-                        })
-                        } else {
-                            res(`\n𝚁𝚎𝚙𝚕𝚢 𝚒𝚖𝚊𝚐𝚎/𝚟𝚒𝚍𝚎𝚘 ${prefix + command}\n`)
-                        }
-            }
-   handler.help = ["stickers"];
-   handler.tags = ["converter"];
-   handler.command = ["take", "steal", "swm"];
-   module.exports = handler;
+
+let handler = async (m, { client, text, prefix, pushname, command, reply }) => {
+    if (!m.quoted) return reply(`Reply to an image/video/GIF\nExample: ${prefix + command} Pack|Author`);
+
+    const quoted = m.quoted;
+    const mime = quoted.mimetype || "";
+
+    const swn = text || pushname;
+    const pcknm = swn.split("|")[0] || packname;
+    const atnm = swn.split("|")[1] || author;
+
+   
+    if (quoted.isAnimated || /gif/.test(mime)) {
+        const media = await quoted.download();
+        return client.sendVideoAsSticker(m.chat, media, m, {
+            packname: pcknm,
+            author: atnm
+        });
+    }
+
+   
+    if (/image/.test(mime)) {
+        const media = await quoted.download();
+        return client.sendImageAsSticker(m.chat, media, m, {
+            packname: pcknm,
+            author: atnm
+        });
+    }
+
+    
+    if (/video/.test(mime)) {
+        if ((quoted.seconds || 0) > 10)
+            return reply("❌ Max video length is 10 seconds");
+
+        const media = await quoted.download();
+        return client.sendVideoAsSticker(m.chat, media, m, {
+            packname: pcknm,
+            author: atnm
+        });
+    }
+
+    reply(`Unsupported media type\nReply image/video/GIF`);
+};
+
+handler.help = ["take", "steal", "swm"];
+handler.tags = ["converter"];
+handler.command = ["take", "steal", "swm"];
+
+module.exports = handler;
